a4cd80b1f0c658772c7cd2fe80beffef3246f0ca
bipashasaha <bipashasaha544@gmail.com>
2026-03-01 23:14:33 +0530

    code update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,57 @@
-$('.testimonial-for').slick({
-  slidesToShow: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.testimonial-nav'
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".testimonialSwiper", {
+    centeredSlides: true,
+    loop: true,
+    speed: 600,
+    watchSlidesProgress: true,
 
-$('.testimonial-nav').slick({
-  slidesToShow: 5,
-  asNavFor: '.testimonial-for',
-  centerMode: true,
-  centerPadding:'0px',
-  focusOnSelect: true,
-  arrows: true
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+
+    on: {
+      slideChange: function () {
+        updateText(this);
+      },
+    },
+  });
+
+  setTimeout(() => {
+    updateText(swiper);
+  }, 0);
+
+  function updateText(swiperInstance) {
+    const realIndex = swiperInstance.realIndex;
+
+    const realSlide = swiperInstance.slides.find(
+      (slide) => slide.getAttribute("data-swiper-slide-index") == realIndex,
+    );
+
+    if (!realSlide) return;
+
+    document.getElementById("testimonialText").textContent =
+      realSlide.dataset.text || "";
+
+    document.getElementById("testimonialName").textContent =
+      realSlide.dataset.name || "";
+
+    document.getElementById("testimonialRole").textContent =
+      realSlide.dataset.role || "";
+  }
 });
